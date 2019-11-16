15ee77f8a38273fe61e61227088ae6fa68ae4aa0
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-15 17:51:26 -0800

    Able to add new users.

--- api/server.js
+++ api/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet')
 
-//Middleware
+const authRouter = require('../auth/authRouter')
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use('/api/auth', authRouter)
 
 server.get('/', (req, res) => {
   res.status(200).json({api: 'Ready to start working'})
---
+++ auth/authRouter.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs');
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/usersModel');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  if (!user.username || !user.password) {
+    res.status(400).json({message: 'Sorry, please enter required information'})
+    return
+  } 
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(error).json(error)
+    })
+});
+// router.post('/register', (req, res) => {
+//   let user = req.body;
+  
+//   if (!user.username || !user.password) {
+//     res.status(400).json({message: 'Sorry, please enter require information.'})
+//     return
+//   }
+
+//   const hash = bcrypt.hashsync(user.password, 10);
+//   user.password = hash;
+
+//   Users.addUser(user)
+//     .then(saved => {
+//       res.status(201).json(saved);
+//     })
+//     .catch(error=> {
+//       res.status(error).json(error)
+//     })
+// })
+
+module.exports = router;
