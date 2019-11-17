131b753aa7861cc7be1d742a8aae99706dd0ddbe
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-17 13:13:11 -0800

    Getting ready to deploy to heroku.

---
+++ data/migrations/20191117113358_ratings.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('ratings', tbl => {
+    tbl.increments()
+    
+    tbl.integer('rating').notNullable();
+
+    tbl
+      .integer('restaurant_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('restaurant')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE')
+
+    tbl
+    .integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('RESTRICT')
+    .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('ratings')
+};
